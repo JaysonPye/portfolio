b3bc961f20e0ff5ac6e8910aa6475dd3d711c9ff
jaysonpye <jaysonpye@hotmail.com>
2025-10-14 14:05:03 +0900

    update layout

--- .eleventy.js
+++ .eleventy.js
@@ -14,27 +14,16 @@ export default function (eleventyConfig) {
     files: "./_site/**/*",
     open: true,
   });
-  eleventyConfig.addCollection("posts", (api) =>
+  eleventyConfig.addCollection("projects", (api) =>
     api
       .getAll()
       .filter(
-        (item) =>
-          Array.isArray(item.data.tags) &&
-          !item.data.eleventyExcludeFromCollections,
+        (i) =>
+          i.data.tags?.includes("project") &&
+          !i.data.eleventyExcludeFromCollections,
       )
       .sort((a, b) => b.date - a.date),
   );
-
-  eleventyConfig.addCollection("tagsList", (api) => {
-    const bad = new Set(["all", "nav"]);
-    const tags = new Set();
-    for (const item of api.getAll()) {
-      (item.data.tags || []).forEach((t) => {
-        if (!bad.has(t)) tags.add(t);
-      });
-    }
-    return [...tags];
-  });
 
   return {
     dir: {
